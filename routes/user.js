882472d6369b923d167218f54c0d2ec8ffd08f4b
Oussama-Zoghlami <oussamazoghlami62@gmail.com>
2025-01-16 23:40:38 +0100

    register and login

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,63 @@ const router =express.Router();
 
 const User = require('../models/user');
 
-router.post('/add',(req , res)=>{
-    data =req.body;
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+
+//singup
+
+router.post('/register', async (req ,res)=>{
+    data = req.body;
     usr = new User(data);
+    salt = bcrypt.genSaltSync(10);
+    cryptedPass = await bcrypt.hashSync( data.password , salt);
+
+    usr.password = cryptedPass;
 
     usr.save()
         .then(
-            (savedUser)=>{
-                res.status(200).send(savedUser)
+            (saved)=>{
+                res.status(200).send(saved)
             }
         )
         .catch(
             (err)=>{
-                res.status(400).send(err)
+                res.send(err)
             }
         )
-});
+})
+
+
+//signin
+router.post('/login' , async (req ,res )=>{
+    data =req.body;
+
+    user = await User.findOne({ email : data.email })
+
+    if(!user){
+        res.status(404).send('email or password invalid !')
+
+    }else{
+        validPass = bcrypt.compareSync(data.password , user.password)
+
+        if(!validPass){
+            res.status(401).send('email or password invalid !!')
+        }else{
+            payload = {
+                _id: user._id,
+                email: user.email,
+                name: user.name
+            }
+            token = jwt.sign( payload , '1234567')
+
+            res.status(200).send({ mytoken :token})
+        }
+    }
+})
+
+
 
 //async
 router.post('/create',async (req ,res)=>{
